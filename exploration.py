1d22d888c0681a8cf2af87acdd08965350851c5e
Apehel <andrelimouzin@gmail.com>
2020-06-13 17:12:17 +0200

    Mise à jour de README.md - description de  l'appli

--- exploration.py
+++ exploration.py
@@ -3,12 +3,17 @@
 Test des moteurs EV3.
 
 Auteur : André-Pierre LIMOUZIN
-Version : 1.0 - 11.2019
+Version : 1.0 - 06.2020
 """
-from robot import Robot, RobotTask
-from explorer import RobotExplorer
+from surveyor import RobotSurveyor, SurveyTask
+from httpd import WebServerTask
+from explorer_tasks import StartStopTask
 
 if __name__ == '__main__':
-    robot = RobotExplorer()
+    robot = RobotSurveyor()
+    WebServerTask(robot)
+    survey = SurveyTask(robot)
+    StartStopTask(robot, survey)
+    #IRControlledTankTask(robot)
+    #robot.surveyTour()
     robot.run()
-
